cd259403ccbdefe86d6e7324fbee21ef4c42aa8a
John Snable <helwyr392@gmail.com>
2018-08-21 11:11:13 -0400

    Add Client model. Add Program model.

---
+++ ModLibrary/Models/Client.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ModLibrary.Models
+{
+    public class Client
+    {
+        [Required]
+        public int id { get; set; }
+
+        [Required]
+        [MaxLength(60)]
+        public string client_name { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string address { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string city { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string state { get; set; }
+
+        [Required]
+        [MaxLength(10)]
+        public string zip_code { get; set; }
+
+        [MaxLength(50)]
+        public string primary_contact { get; set; }
+
+        [MaxLength(15)]
+        public string phone { get; set; }
+
+        [MaxLength(10)]
+        public string phone_ext { get; set; }
+
+        [MaxLength(50)]
+        public string email { get; set; }
+
+        [MaxLength(20)]
+        public string dot_net_ver { get; set; }
+
+        [MaxLength(20)]
+        public string macola_ver { get; set; }
+
+        [MaxLength(20)]
+        public string sage_ver { get; set; }
+
+        [MaxLength(20)]
+        public string wisys_ver { get; set; }
+
+        [MaxLength(20)]
+        public string crystal_ver { get; set; }
+
+        [MaxLength(20)]
+        public string office_ver { get; set; }
+
+        [MaxLength(20)]
+        public string sql_ver { get; set; }
+
+        [MaxLength(20)]
+        public string os_ver { get; set; }
+
+        [MaxLength(20)]
+        public string third_party_ver { get; set; }
+
+        public int company_recID { get; set; }
+
+        [MaxLength(20)]
+        public string company_ID { get; set; }
+    }
+}
---
+++ ModLibrary/Models/Programs.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ModLibrary.Models
+{
+    public class Programs
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public int client_recID { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string program_name { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string file_path { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string environment { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string current_version { get; set; }
+        public DateTime modified_date { get; set; }
+
+        [MaxLength(50)]
+        public string framework { get; set; }
+
+        [MaxLength(100)]
+        public string Type { get; set; }
+
+        [MaxLength(150)]
+        public string DBConnection { get; set; }
+
+        [MaxLength(150)]
+        public string RanBy { get; set; }
+
+        [MaxLength(200)]
+        public string LiveLocation { get; set; }
+    }
+}
